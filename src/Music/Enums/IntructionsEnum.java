e59444535a4421416d170319b55292552a65d97f
rhpetry <rafaelhpetry@gmail.com>
2021-03-29 21:25:51 -0300

    rename instruction enum

--- src/Music/Enums/IntructionsEnum.java
+++
@@ -1,6 +0,0 @@
-package Music.Enums;
-
-public enum IntructionsEnum {
-    BPM_UP, BPM_DOWN, VOL_UP, VOL_DOWN, OCTAVE_UP, OCTAVE_DOWN, CHANGE_INST,
-    A, B, C, D, E, F, G, PAUSE, REPEAT_NOTE, RANDOM_NOTE
-}
