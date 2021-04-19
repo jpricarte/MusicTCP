add25e5f58e641411dd5847e2f75589eb85dae66
Jordi Pujol Ricarte <jpricarte@inf.ufrgs.br>
2021-04-19 14:14:06 -0300

    Criando interfaces

---
+++ src/gui/UserInterface.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class UserInterface {
+    private JButton gerarMúsicaButton;
+    private JSlider slider1;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JProgressBar progressBar1;
+    private JButton tocarButton;
+    private JButton salvarMúsicaButton;
+    private JButton importarTextoButton;
+    private JTextArea textArea1;
+
+}
