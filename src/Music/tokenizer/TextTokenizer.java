ca6d265e679ceb00493242b607f1d048c409a386
rhpetry <rafaelhpetry@gmail.com>
2021-04-20 10:47:20 -0300

    changed key mappings

--- src/Music/tokenizer/TextTokenizer.java
+++ src/Music/tokenizer/TextTokenizer.java
@@ -3,7 +3,6 @@ package Music.tokenizer;
 import java.util.List;
 import java.util.ArrayList;
 import Music.Enums.InstructionEnum;
-import Music.musicState.MusicState;
 
 public class TextTokenizer {
 
@@ -18,7 +17,7 @@ public class TextTokenizer {
     }
 
     private void tokenizeMusic(String text) {
-        this.tokens = new ArrayList<InstructionEnum>();
+        this.tokens = new ArrayList<>();
         while (text.length() > 0){
             boolean found_instruction=false;
             for (InstructionEnum instruction : InstructionEnum.values()) {
@@ -31,28 +30,13 @@ public class TextTokenizer {
                         break;
                     }
                 }
-                if (found_instruction == true)
+                if (found_instruction)
                     break;
             }
             if (!found_instruction) {
                 text = text.substring(1);
+                tokens.add(InstructionEnum.REPEAT_NOTE);
             }
-        }
-    }
-
-    public static void main (String[] args) {
-        TextTokenizer t = new TextTokenizer("A B C DEFG");
-        List<InstructionEnum> l = t.getTokens();
-        for (InstructionEnum e : l) {
-            System.out.print(e.getName()[0]);
-        }
-        MusicState state = new MusicState();
-        state.setVolumeConstraints(0, 20, 2, 2);
-        state.setOctaveConstraints(1, 4, 1, 1);
-        state.setBPMConstraints(40, 400, 10, 80);
-        System.out.println("");
-        for (InstructionEnum e : l) {
-            System.out.print(e.getTranslation(state));
         }
     }
 }
